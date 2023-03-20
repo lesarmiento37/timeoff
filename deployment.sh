4bb0307c69dd6784b9e1d68abce3885dc60e80fa
carlos.sarmiento <carlos.sarmiento@omnilatam.com>
2023-03-20 00:30:18 -0500

    Adding minikube configuration

--- deployment.sh
+++ deployment.sh
@@ -1,9 +1,15 @@
 #!bin/bash
-CURRENT_VERSION=$(docker images | grep leonardos37/timeoff | awk '{print $2}' | cut -c 2-4)
-NEW_VERSION=$(echo "$CURRENT_VERSION + 1" | bc)
+IMAGE=$(kubectl get deploy/timeoff -o jsonpath="{..image}{'\n'}")
+echo image
+echo $IMAGE
+echo current Version
+CURRENT_VERSION=$(echo $IMAGE| cut -c 22-25)
+echo $CURRENT_VERSION
+NEW_VERSION=$(echo "$CURRENT_VERSION + 0.1" | bc)
 docker rmi timeoff:v$CURRENT_VERSION
 echo "New Version $NEW_VERSION"
 docker build -t timeoff:v$NEW_VERSION ./
 docker image tag timeoff:v$NEW_VERSION leonardos37/timeoff:v$NEW_VERSION
 docker image push leonardos37/timeoff:v$NEW_VERSION
-docker-compose up
+kubectl -n default --record deployment.apps/timeoff set image deployment.v1.apps/timeoff timeoff=lesarmiento37/timeoff:v$NEW_VERSION
+#docker-compose up
